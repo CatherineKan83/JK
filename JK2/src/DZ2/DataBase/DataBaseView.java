dc212178e9907468e5aeee383f7ccfd24265d343
CatherineKan83 <kankatya@bk.ru>
2024-09-18 02:02:17 +0500

    dz2

---
+++ JK2/src/DZ2/DataBase/DataBaseView.java
@@ -0,0 +1,13 @@
+package DZ2.DataBase;
+
+
+public interface DataBaseView {
+
+    String showMessageDataBase(String message);
+
+    void writeLog(String txt);
+
+    String sendLogsToServer();
+
+    String sendLogsToClient();
+}
